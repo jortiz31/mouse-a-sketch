d89008889b1df6d3825ce9ff99760841a09af0b0
Jonathan <jortiz@tunein.com>
2016-03-18 13:29:01 -0700

    base.js

--- mouse_a_sketch/base.js
+++ mouse_a_sketch/base.js
@@ -1,5 +1,8 @@
 console.log("this is a sanity check");
 
 $(document).ready(function(){
-  console.log("Let's get coding!")
+  $( "td" ).on( "mouseover", function() {
+    $( this ).css( "background-color", $("input").val() );
+  });
+
 })
